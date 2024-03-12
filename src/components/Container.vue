213e3c1640ceb4a6d1c3713e7e61e04f6511111b
GitSojin <dfsa356@naver.com>
2024-03-13 05:11:18 +0900

    뷰 인스타그램 프로젝트 생성 & 레이아웃 만들기

---
+++ src/components/Container.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <Post :PostData="PostData" />
+  </div>
+</template>
+<script>
+import Post from "./Post.vue";
+export default {
+  components: { Post: Post },
+  props: {
+    PostData: Array,
+  },
+};
+</script>
+<style></style>
